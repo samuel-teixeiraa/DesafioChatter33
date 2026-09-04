178c44598b5eeaa5dd1af6ee7498bb4da8b2c704
samuel-teixeiraa <samuelcardosoteixeira@gmail.com>
2026-09-04 09:54:59 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -18,6 +18,9 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseDefaultFiles();
+
 app.UseStaticFiles(); // Importante para ler a pasta wwwroot (CSS, JS, etc.)
 
 app.UseRouting();
